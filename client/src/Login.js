61e88e1c16b146a7c76e564ccf64333dd109307b
Teressa Sung <teressas.hawaii@gmail.com>
2022-02-17 10:58:43 -0800

    changed redirect_uri

--- client/src/Login.js
+++ client/src/Login.js
@@ -7,7 +7,7 @@ import { Container } from 'react-bootstrap'
 const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "6a132f4de6684e5abde7622e54a88af0";
-const redirectUri = "http://18.117.95.56";
+const redirectUri = "http://3.145.157.27";
 const scopes = [
     "streaming",
     "user-read-email",
