32f3046385690d9217a1c9142bc6284cbf3392e4
Teressa Sung <teressas.hawaii@gmail.com>
2022-02-18 12:54:14 -0800

    changed ip

--- client/src/Login.js
+++ client/src/Login.js
@@ -7,7 +7,7 @@ import { Container } from 'react-bootstrap'
 const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "6a132f4de6684e5abde7622e54a88af0";
-const redirectUri = "http://18.223.151.44";
+const redirectUri = "http://3.134.97.69";
 const scopes = [
     "streaming",
     "user-read-email",
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // call .env file 
 require('dotenv').config()
+console.log("line3",process.env)
 
 // import express, cors, bodyParser, lyricsFinder and SpotifyWebApi library
 const express = require('express')
@@ -48,19 +49,19 @@ app.post('/api/refresh', (req, res) => {
 // create login route to create new instance of the SpotifyWebApi and pass in the authentication parameters
 app.post('/api/login', (req, res) => {
     const code = req.body.code
-    console.log(code)
+    console.log("line52",code)
     const spotifyApi = new SpotifyWebApi({
         redirectUri: process.env.REDIRECT_URI,
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET
     })
-    console.log(spotifyApi)
+    console.log("line58",spotifyApi)
 
     // from spotify-web-api-node documentation, 
     // authorizes that we have a code then provides the tokens we need to do the accessing of the code that we need to refresh authorization
     spotifyApi
         .authorizationCodeGrant(code)
-        console.log(code)
+        console.log("line64",code)
         .then(data => {
             // call access token, refresh token and expiresIn from API call
             res.json({
@@ -68,9 +69,12 @@ app.post('/api/login', (req, res) => {
                 refreshToken: data.body.refresh_token,
                 expiresIn: data.body.expires_in
             })
+            console.log("line72ACG",accessToken)
+            console.log("line73ACG",refreshToken)
+            console.log("line74ACG",expiresIn)
         })
         .catch(err => {
-            console.log(err)
+            console.log("line77",err)
             res.sendStatus(400)
         })
 })
