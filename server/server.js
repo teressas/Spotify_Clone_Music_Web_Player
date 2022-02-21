ad1a666ce89025f5d8eb58e2808acb95df11c22c
Teressa Sung <teressas.hawaii@gmail.com>
2022-02-20 23:17:24 -0800

    changed ip

--- server/server.js
+++ server/server.js
@@ -26,9 +26,9 @@ app.post('/api/refresh', (req, res) => {
     // get refresh token from
     const refreshToken = req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
+        redirectUri: process.env.REACT_APP_REDIRECT_URI,
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        clientSecret: process.env.REACT_APP_CLIENT_SECRET,
         refreshToken,
     })
     console.log(spotifyApi)
@@ -53,9 +53,9 @@ app.post('/api/login', (req, res) => {
     const code = req.body.code
     // console.log("login",process.env.CLIENT_ID)
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: process.env.REDIRECT_URI,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET
+        redirectUri: process.env.REACT_APP_REDIRECT_URI,
+        clientId: process.env.REACT_APP_CLIENT_ID,
+        clientSecret: process.env.REACT_APP_CLIENT_SECRET
     })
     console.log(spotifyApi)
     // from spotify-web-api-node documentation, 
