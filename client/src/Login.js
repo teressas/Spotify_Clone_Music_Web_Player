b76f7f80bf251a2b470511f77848295764a88300
Teressa Sung <teressas.hawaii@gmail.com>
2022-02-21 12:07:34 -0800

    changed ip

--- client/src/Login.js
+++ client/src/Login.js
@@ -7,7 +7,7 @@ import { Container } from 'react-bootstrap'
 const authEndpoint = "https://accounts.spotify.com/authorize";
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "6a132f4de6684e5abde7622e54a88af0";
-const redirectUri = "http://3.12.165.227";
+const redirectUri = "http://3.16.67.74";
 const scopes = [
     "streaming",
     "user-read-email",
